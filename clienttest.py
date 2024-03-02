f0830cc00114cd00cd78bd45d425f7061b84deef
root <lynchdc39987@gmail.com>
2024-03-02 02:27:16 +0000

    initialtest1

--- clienttest.py
+++ clienttest.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 
 from inventree.api import InvenTreeAPI
+
 import os
+
+from prestashop.client import Client
+
 
 token = os.getenv("INVENTREE_API")
 server = os.getenv("INVENTREE_SERVER")
 api = InvenTreeAPI(server, token=token)
 
-print(api.server_details)
+
+
+from prestashop.client import Client
+
+pstoken = os.getenv("PS_TOKEN")
+psserver = os.getenv("PS_SERVER")
+client = Client(pstoken,psserver)
+
+
+
 
