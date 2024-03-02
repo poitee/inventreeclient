d8858daa6a7239da44300aceadecd43d95edec5e
root <lynchdc39987@gmail.com>
2024-03-02 04:21:30 +0000

    tired is tired try when untired

--- clienttest.py
+++ clienttest.py
@@ -10,10 +10,9 @@ from prestashop.client import Client
 token = os.getenv("INVENTREE_API")
 server = os.getenv("INVENTREE_SERVER")
 api = InvenTreeAPI(server, token=token)
-
-
-
+from invantree.part import part
 from prestashop.client import Client
+import yaml
 
 pstoken = os.getenv("PS_TOKEN")
 psserver = os.getenv("PS_SERVER")
